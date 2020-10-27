3859896c9d57cb3e246a1b3fb2435176db05837c
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-10-27 21:34:23 +0100

    @public components

--- src/app/@public/pages/public.module.ts
+++ src/app/@public/pages/public.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
+import { HeaderComponent } from '@shop-core/component/header/header.component';
+import { NavbarComponent } from '@shop-core/component/navbar/navbar.component';
+import { FooterComponent } from '@shop/core/component/footer/footer.component';
 
 
 @NgModule({
-  declarations: [PublicComponent],
+  declarations: [PublicComponent, HeaderComponent, NavbarComponent,FooterComponent],
   imports: [
     CommonModule,
     PublicRoutingModule
