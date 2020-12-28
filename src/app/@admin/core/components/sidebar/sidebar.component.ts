10157687e07baaffe1cdee0ac676b34fe93a1cfd
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-12-28 12:58:39 +0100

    sideBar admin menu from json

--- src/app/@admin/core/components/sidebar/sidebar.component.ts
+++ src/app/@admin/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import menuSidebar from '@data/menus/adminMenu.json'
+import { IMenuAdmin } from '../../../../@public/core/Interfaces/IMenuAdmin';
 
 @Component({
   selector: 'app-admin-sidebar',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+
+  menuItems: Array<IMenuAdmin> = menuSidebar;
 
   constructor() { }
 
