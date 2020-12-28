1c2cf5f80b5b272164efc472aa97df420547c716
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-12-28 13:43:47 +0100

    formsModule and ngpaginationModule

--- src/app/@public/pages/games/games.module.ts
+++ src/app/@public/pages/games/games.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { GamesRoutingModule } from './games-routing.module';
 import { GamesComponent } from './games.component';
+import { ProductCategoriListModule } from '../../../@shared/product-categori-list/product-categori-list.module';
+import { FormsModule } from '@angular/forms';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [GamesComponent],
   imports: [
     CommonModule,
-    GamesRoutingModule
+    GamesRoutingModule,
+    ProductCategoriListModule,
+    FormsModule,
+    NgbPaginationModule
   ]
 })
 export class GamesModule { }
