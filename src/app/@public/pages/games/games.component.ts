37232708d9065a6d2d5e5daa555265b6c9b2e20c
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-12-28 14:49:47 +0100

    pagination

--- src/app/@public/pages/games/games.component.ts
+++ src/app/@public/pages/games/games.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '@mugan86/ng-shop-ui/lib/interfaces/product.interface';
+import { ACTIVE_FILTERS } from 'src/app/@shared/constants/filter';
+import { ProductService } from '../../../services/product.service';
+import { IInfoPage } from '../../core/Interfaces/IResultData';
 
 @Component({
   selector: 'app-games',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
 
-  constructor() { }
+  title = 'Título de la categoría';
+  productList: Array<IProduct> = [];
+  selectPage;
+  infoPage: IInfoPage = {
+    page: 1,
+    pages: 8,
+    total: 160,
+    itemsPerPage: 20
+  }
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.loadData()
   }
+  
+  loadData() {
+    this.productService.getByPlatform(this.infoPage.page,this.infoPage.itemsPerPage, ACTIVE_FILTERS.ACTIVE, '18' ,false, true ).subscribe((data) => {
+      this.productList = data.result;
+      this.infoPage = data.info
+    })
+  };
+
 
 }
