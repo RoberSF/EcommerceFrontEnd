1b910857461b41d8f36f8be24c30611fd101e17b
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-12-29 13:27:35 +0100

    getByPlatofmrs

--- src/app/@public/pages/games/games.component.ts
+++ src/app/@public/pages/games/games.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduct } from '@mugan86/ng-shop-ui/lib/interfaces/product.interface';
+import { IGamePageInfo } from '@shop/core/Interfaces/IGamesPageInfo';
 import { ACTIVE_FILTERS } from 'src/app/@shared/constants/filter';
+import { GAMES_PAGES_INFO } from 'src/app/@shared/constants/game.constants';
 import { ProductService } from '../../../services/product.service';
 import { IInfoPage } from '../../core/Interfaces/IResultData';
+import { TYPE_OPERATION } from '../../../@shared/constants/game.constants';
 
 @Component({
   selector: 'app-games',
@@ -20,18 +24,28 @@ export class GamesComponent implements OnInit {
     total: 160,
     itemsPerPage: 20
   }
+  gamesPageInfo: IGamePageInfo;
+  typeData: TYPE_OPERATION;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.loadData()
+    this.activatedRoute.params.subscribe( (params) => {
+      this.typeData = params.type;
+      this.selectPage = 1; // Cada vez que se cargue la página empezamos a contar desde la pag 1
+      this.gamesPageInfo = GAMES_PAGES_INFO[`${params.type}/${params.filter}`]
+      this.loadData() //cada vez que cambiemos de página haremos un "refresh" de la data
+    })
   }
   
   loadData() {
-    this.productService.getByPlatform(this.infoPage.page,this.infoPage.itemsPerPage, ACTIVE_FILTERS.ACTIVE, '18' ,false, true ).subscribe((data) => {
-      this.productList = data.result;
-      this.infoPage = data.info
-    })
+    if( this.typeData === TYPE_OPERATION.PLATFORMS) {
+
+      this.productService.getByPlatform(this.selectPage,this.infoPage.itemsPerPage, ACTIVE_FILTERS.ACTIVE, this.gamesPageInfo.platformsIds ,false, true, true).subscribe((data) => {
+        this.productList = data.result;
+        this.infoPage = data.info
+      })
+    }
   };
 
 
