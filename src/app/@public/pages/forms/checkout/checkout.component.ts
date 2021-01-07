b213d64888bc671a103a19e7351fab1e5fb9936b
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-07 23:39:33 +0100

    working in payment

--- src/app/@public/pages/forms/checkout/checkout.component.ts
+++ src/app/@public/pages/forms/checkout/checkout.component.ts
@@ -4,6 +4,9 @@ import { AuthService } from '../../../../services/auth.service';
 import { Router } from '@angular/router';
 import { environment } from '../../../../../environments/environment.prod';
 import { StripePaymentService } from '@mugan86/stripe-payment-form';
+import { take } from 'rxjs/internal/operators/take';
+import { ShoppingCartService } from '../../../../services/shopping-cart.service';
+import { infoEventAlert } from 'src/app/@shared/alerts/alerts';
 
 @Component({
   selector: 'app-checkout',
@@ -16,8 +19,12 @@ export class CheckoutComponent implements OnInit {
   key = environment.stripePublicKey;
   address = '';
 
-  constructor(private authService: AuthService, private router: Router, private stripePaymentService: StripePaymentService) {
-    this.authService.accessVar$.subscribe( (data: IMeData) => {
+  constructor(private authService: AuthService, 
+    private router: Router, 
+    private stripePaymentService: StripePaymentService,
+    private shoppingCartService: ShoppingCartService) {
+    
+    this.authService.accessVar$.subscribe( (data: IMeData) => { 
       // Comprobamos el status para validación y o redirigir
       if ( !data.status) {
           this.router.navigate( ['/login']);
@@ -26,20 +33,31 @@ export class CheckoutComponent implements OnInit {
       this.meData = data;
     })
 
-    this.stripePaymentService.cardTokenVar$.subscribe( (token: string) => {
+    this.stripePaymentService.cardTokenVar$.pipe(take(1)).subscribe( (token: string) => { //take() hace que sólo se ejecute una vez, si no puede que el pago se hiciera varias veces
       console.log(token);
       if ( token.indexOf('tok_') > -1 && this.meData.status && this.address !== '') {
           // Podemos enviar los datos
           console.log('Podemos enviar la info');
+          // Descripción del pedido. Tenemos que crear función en el carrito
+          // Divisa
+          // Cliente de stripe
+          // Total a pagar
       }
     })
    }
 
   ngOnInit() {
     this.authService.start();
+    this.shoppingCartService.initializeCart();
   }
 
   sendData() {
+    // Si no soy cliente de stripe no puede hacer el pago
+    if ( this.meData.user.stripeCustomer === null ) {
+      // Alerta para mostrar info
+      infoEventAlert('Cliente no existe', 'Necesitamos un cliente para realizar el pago')
+      return
+    }
     this.stripePaymentService.takeCardToken(true);
   }
 
