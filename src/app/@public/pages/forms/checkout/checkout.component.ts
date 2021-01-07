21c951750033e6dd86314a21b1174d3e425d9707
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-07 19:45:50 +0100

    data and valdiators to payment

--- src/app/@public/pages/forms/checkout/checkout.component.ts
+++ src/app/@public/pages/forms/checkout/checkout.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { IMeData } from '../../../core/Interfaces/ISession';
 import { AuthService } from '../../../../services/auth.service';
 import { Router } from '@angular/router';
+import { environment } from '../../../../../environments/environment.prod';
+import { StripePaymentService } from '@mugan86/stripe-payment-form';
 
 @Component({
   selector: 'app-checkout',
@@ -11,8 +13,10 @@ import { Router } from '@angular/router';
 export class CheckoutComponent implements OnInit {
 
   meData: IMeData;
+  key = environment.stripePublicKey;
+  address = '';
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private stripePaymentService: StripePaymentService) {
     this.authService.accessVar$.subscribe( (data: IMeData) => {
       // Comprobamos el status para validación y o redirigir
       if ( !data.status) {
@@ -21,10 +25,22 @@ export class CheckoutComponent implements OnInit {
       }
       this.meData = data;
     })
+
+    this.stripePaymentService.cardTokenVar$.subscribe( (token: string) => {
+      console.log(token);
+      if ( token.indexOf('tok_') > -1 && this.meData.status && this.address !== '') {
+          // Podemos enviar los datos
+          console.log('Podemos enviar la info');
+      }
+    })
    }
 
   ngOnInit() {
     this.authService.start();
+  }
+
+  sendData() {
+    this.stripePaymentService.takeCardToken(true);
   }
 
 }
