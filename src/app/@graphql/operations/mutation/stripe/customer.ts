b1f30090b954700cd512dbf1b337c336c9f3f1d9
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-08 13:54:30 +0100

    .

--- src/app/@graphql/operations/mutation/stripe/customer.ts
+++ src/app/@graphql/operations/mutation/stripe/customer.ts
@@ -2,21 +2,6 @@ import gql from 'graphql-tag';
 import { USER_FRAGMENT } from '@graphql/operations/fragment/user';
 
 
-
-// export const CREATE_CUSTOMER = gql`
-
-// mutation addUser($user: UserInput!, $include: Boolean!) {
-//     register(user: $user) {
-//       status
-//       message
-//       user {
-//         ...UserObject
-//       }
-//     }
-//   }
-//   ${ USER_FRAGMENT }
-//   `;
-
 export const CREATE_CUSTOMER = gql` 
 
 mutation createClient($name: String!, $email: String!) {
