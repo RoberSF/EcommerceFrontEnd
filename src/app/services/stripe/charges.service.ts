d79408e2b40379a9cd12a8d2c1b5b0712415105b
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-09 11:29:00 +0100

    chargesByList added graphql

--- src/app/services/stripe/charges.service.ts
+++ src/app/services/stripe/charges.service.ts
@@ -4,6 +4,7 @@ import { ApiService } from '@graphql/services/api.service';
 import { IPayment } from '../../@public/core/Interfaces/stripe/IStripeDescription';
 import { STRIPE_PAYMENT } from '../../@graphql/operations/mutation/stripe/charges';
 import { map } from 'rxjs/internal/operators/map';
+import { CHARGES_CUSTOMER_LIST_QUERY } from '@graphql/operations/query/stripe/charges';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,13 @@ pay(payment: IPayment) {
     return this.set(STRIPE_PAYMENT, {payment}).pipe(map( (result: any) => {
             return result.chargeOrder
     }))
+}
 
+chargesListByCustomer(customer: string, limit: number, startingAfter: string, endingBefore: string) {
+  return this.get(CHARGES_CUSTOMER_LIST_QUERY, {customer, limit, startingAfter, endingBefore}).pipe(map( (result: any) => {
+    return result.chargesByCustomer
+  }))
 }
 
 }
+
