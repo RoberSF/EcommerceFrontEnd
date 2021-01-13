e41b88c003bcfb7381a50de12a5b4c5f60e322c7
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-13 18:45:33 +0100

    header refactor

--- src/app/@public/core/component/header/header.component.ts
+++ src/app/@public/core/component/header/header.component.ts
@@ -13,14 +13,14 @@ export class HeaderComponent implements OnInit {
     status: false,
   };
   access = false;
-  role: string;
+  role: string = '';
   userLabel: string =  ''
 
   constructor(private auth: AuthService) {
     this.auth.accessVar$.subscribe( (result) => {
       this.session = result;
       this.access = this.session.status;
-      this.role = this.session.user.role;
+      //this.role = this.session.user.role;
       this.userLabel = `${ this.session.user?.email }`;
     });
    }
